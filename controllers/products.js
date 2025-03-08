a73f813628c4d4772b6167cd097f5b57c388b99e
bhushantbn <bhushantbn@gmail.com>
2025-03-08 18:05:38 +0530

    added sort,select and pagination methods

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-  const { company, name } = req.query;
+  const { company, name, sort, select } = req.query;
   const queryObject = {};
   if (company) {
     queryObject.company = company;
@@ -9,14 +9,26 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+  let apiData = product.find(queryObject);
+  if (sort) {
+    let sortFix = sort.replace(",", " ");
+    apiData = apiData.sort(sortFix);
+  }
   console.log(queryObject);
-
-  const myData = await product.find({});
-  res.status(200).json({ myData });
+  if (select) {
+    let selectFix = select.split(",").join(" ");
+    apiData = apiData.select(selectFix);
+  }
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 5;
+  let skip = (page - 1) * limit;
+  apiData = apiData.skip(skip).limit(limit);
+  const myData = await apiData;
+  res.status(200).json({ myData, perPage: myData.length });
 };
 
 const getAllProductsTesting = async (req, res) => {
-  const myData = await product.find(req.query);
+  const myData = await product.find(req.query).select("name");
   res.status(200).json({ myData });
 };
 module.exports = { getAllProducts, getAllProductsTesting };
